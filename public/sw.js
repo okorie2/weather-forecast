aca956ffea06698cef5780d5249b79d0a7ecba52
okorie2 <44427128+okorie2@users.noreply.github.com>
2020-09-11 11:06:38 +0100

    :)

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 
-const dynamicCacheName = 'site-dynamic-v1';
+/*const dynamicCacheName = 'site-dynamic-v1';
 // activate event
 self.addEventListener('activate', evt => {
   evt.waitUntil(
@@ -15,12 +15,37 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
-          return fetchRes;
-        })
+      return cacheRes || fetch(evt.request).then(async fetchRes => {
+        const cache = await caches.open(dynamicCacheName);
+        cache.put(evt.request.url, fetchRes.clone());
+        return fetchRes;
       });
     })
   );
+});*/
+self.addEventListener('install', (event) => {
+  //...
 });
+self.addEventListener('activate', (event) => {
+  //...
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) { //entry found in cache
+          return response
+        }
+        return fetch(event.request)
+      }
+    )
+  )
+})
+navigator.serviceWorker.ready.then((swRegistration) => {
+  return swRegistration.sync.register('event1')
+});
+self.addEventListener('sync', (event) => {
+  if (event.tag == 'event1') {
+    event.waitUntil(doSomething())
+  }
+})
